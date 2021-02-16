e5620c5beb1940d45e70f33fc899cf9aa1ed418b
DESKTOP-88QG4I0\cjy <cpcp127@jetserve.net>
2021-02-16 16:17:42 +0900

    생성자

--- lib/alertbox.dart
+++ lib/alertbox.dart
@@ -5,11 +5,22 @@ class AlertBox extends StatefulWidget {
   _AlertBoxState createState() => _AlertBoxState();
 }
 
+class AlertItemModel {
+  String category;
+  String title;
+  String timeAgo;
+
+  AlertItemModel({this.category, this.title, this.timeAgo});
+}
+
 class _AlertBoxState extends State<AlertBox> {
-  bool isSelected=false;
-  bool isSelected1=false;
-  bool isSelected2=false;
-  bool isSelected3=false;
+
+  List<AlertItemModel> items = [
+    AlertItemModel(category: "[ 마실나온김에 카페 ]", title: "누군가 나의 게시글에 댓글을 달았어요 💬", timeAgo: "12분 전"),
+    AlertItemModel(category: "[ 마실나온김에 일상 ]", title: "나의 댓글에 답글이 달렸어요👤💬", timeAgo: "30분 전"),
+    AlertItemModel(category: "[ 고객센터 ]", title: "문의주신 내용에 답변이 도착했어요", timeAgo: "40분 전"),
+    AlertItemModel(category: "[ 마실 ]", title: "알량땅구님 안녕하세요 ", timeAgo: "1시간 전"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,123 +34,17 @@ class _AlertBoxState extends State<AlertBox> {
         Text('소식함', style: TextStyle(fontSize: 16, color: Colors.black)),
         leading: BackBtn(),
       ),
-      body: Column(
-        children: <Widget>[
-          firstAlert(),
-          secondAlert(),
-          thirdAlert(),
-          lastAlert(),
-        ],
-      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return AlertItem(
+            category: items[index].category,
+            title: items[index].title,
+            timeAgo: items[index].timeAgo,
+          );
+        }
+      )
     );
-  }
-
-  Container firstAlert() {
-    return Container(
-      height: 63,
-      decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Color(0xFFeeeeee))),),
-      child: ListTile(
-        onTap: (){
-          setState(() {
-            isSelected=!isSelected;
-          });
-        },
-        tileColor: isSelected?Color(0xFFfff0f0):Colors.white,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 16,top: 11),
-          child: Text('[마실나온김에 카페]',style: TextStyle(color: Color(0xFF444444),fontSize: 13),),
-        ),
-        subtitle: Padding(
-          padding: const EdgeInsets.only(left: 16,bottom: 12),
-          child: Text('누군가 나의 게시글에 댓글을 달았어요💬',style: TextStyle(color: Color(0xFF909090),fontSize: 12)),
-        ),
-        trailing: Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: Text('12분 전',style: TextStyle(color: Color(0xFF909090),fontSize: 10)),
-        ),
-      ),
-    );
-  }
-
-  Container secondAlert() {
-    return Container(
-          height: 63,
-          decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Color(0xFFeeeeee))),),
-          child: ListTile(
-            onTap: (){
-              setState(() {
-                isSelected1=!isSelected1;
-              });
-            },
-            tileColor: isSelected1?Color(0xFFfff0f0):Colors.white,
-            title: Padding(
-              padding: const EdgeInsets.only(left: 16,top: 11),
-              child: Text('[마실나온김에 일상]',style: TextStyle(color: Color(0xFF444444),fontSize: 13),),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(left: 16,bottom: 12),
-              child: Text('나의 댓글에 답글이 달렸어요👤💬',style: TextStyle(color: Color(0xFF909090),fontSize: 12)),
-            ),
-            trailing: Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Text('30분 전',style: TextStyle(color: Color(0xFF909090),fontSize: 10)),
-            ),
-          ),
-        );
-  }
-
-  Container thirdAlert() {
-    return Container(
-      height: 63,
-      decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Color(0xFFeeeeee))),),
-      child: ListTile(
-        onTap: (){
-          setState(() {
-            isSelected2=!isSelected2;
-          });
-        },
-        tileColor: isSelected2?Color(0xFFfff0f0):Colors.white,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 16,top: 11),
-          child: Text('[고객센터]',style: TextStyle(color: Color(0xFF444444),fontSize: 13),),
-        ),
-        subtitle: Padding(
-          padding: const EdgeInsets.only(left: 16,bottom: 12),
-          child: Text('문의주신 내용에 답변이 도착했어요',style: TextStyle(color: Color(0xFF909090),fontSize: 12)),
-        ),
-        trailing: Padding(
-          padding: const EdgeInsets.only(bottom: 10),
-          child: Text('40분 전',style: TextStyle(color: Color(0xFF909090),fontSize: 10)),
-        ),
-      ),
-    );
-  }
-
-  Container lastAlert() {
-    return Container(
-          height: 63,
-          decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Color(0xFFeeeeee))),),
-          child: ListTile(
-            onTap: (){
-              setState(() {
-                isSelected3=!isSelected3;
-              });
-            },
-            tileColor: isSelected3?Color(0xFFfff0f0):Colors.white,
-            title: Padding(
-              padding: const EdgeInsets.only(left: 16,top: 11),
-              child: Text('[마실]',style: TextStyle(color: Color(0xFF444444),fontSize: 13),),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(left: 16,bottom: 12),
-              child: Text('알량땅꾸님 안녕하세요',style: TextStyle(color: Color(0xFF909090),fontSize: 12)),
-            ),
-            trailing: Padding(
-              padding: const EdgeInsets.only(bottom: 10),
-              child: Text('1시간 전',style: TextStyle(color: Color(0xFF909090),fontSize: 10)),
-            ),
-          ),
-        );
   }
 }
 
@@ -157,3 +62,49 @@ class BackBtn extends StatelessWidget {
         child: Container(width:44,height:44,child: SvgPicture.asset('assets/images/clickable_btn_arrow.svg')));
   }
 }
+
+class AlertItem extends StatefulWidget {
+
+  final String category;
+  final String title;
+  final String timeAgo;
+
+  const AlertItem({Key key, this.category, this.title, this.timeAgo}) : super(key: key);
+
+  @override
+  _AlertItemState createState() => _AlertItemState();
+}
+
+class _AlertItemState extends State<AlertItem> {
+
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 63,
+      decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Color(0xFFeeeeee))),),
+      child: ListTile(
+        onTap: (){
+          setState(() {
+            isSelected=!isSelected;
+          });
+        },
+        tileColor: isSelected?Color(0xFFfff0f0):Colors.white,
+        title: Padding(
+          padding: const EdgeInsets.only(left: 16,top: 11),
+          child: Text(widget.category,style: TextStyle(color: Color(0xFF444444),fontSize: 13),),
+        ),
+        subtitle: Padding(
+          padding: const EdgeInsets.only(left: 16,bottom: 12),
+          child: Text(widget.title,style: TextStyle(color: Color(0xFF909090),fontSize: 12)),
+        ),
+        trailing: Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: Text(widget.timeAgo,style: TextStyle(color: Color(0xFF909090),fontSize: 10)),
+        ),
+      ),
+    );
+  }
+}
+
